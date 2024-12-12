7146710a968ab4e715e3bc43f1e9e923a21d9a9c
Natsukiraa <gazio.mael@gmail.com>
2024-12-12 10:39:58 +0100

    [TEST 🛠️]] Added integration tests

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,5 +1,21 @@
 import pytest
+import requests
 
 @pytest.mark.integration
 def test_integration():
     assert True
+
+@pytest.mark.integration
+def test_endpointHello():
+    data = requests.get('http://localhost:5000/')
+    assert data.text == "Welcome to the API!"
+
+@pytest.mark.integration
+def test_getData():
+    data = requests.get('http://localhost:5000/api/data')
+    assert data.json() == {
+        'name': 'John Doe',
+        'age': 30,
+        'occupation': 'Software Developer'
+    }
+    assert data.status_code == 200
